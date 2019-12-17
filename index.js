188c73ba6e5334e96063409960207fb0a89209a7
Mike Sinnema <msinnema33@gmail.com>
2019-12-17 09:19:41 -0700

    first two done

--- index.js
+++ index.js
@@ -73,8 +73,8 @@ function processLength(list, callback) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
  */
-function processLastItem( /* CODE HERE */ ) {
-    /* CODE HERE */
+function processLastItem(stringList, callback) {
+    return callback(stringList[stringList.length - 1])
 }
 
 /**
